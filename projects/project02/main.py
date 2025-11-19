ba7d5b1e098f2faafb30fa0fb8c508f009973e3a
Ariyana <germantranslator848@gmail.com>
2025-11-19 07:59:03 +0330

    Update Project02 main.py to auto-run without input

--- projects/project02/main.py
+++ projects/project02/main.py
@@ -1,6 +1,12 @@
 import qrcode
 
-data = input("Enter text or URL: ")
+# متن یا URL
+data = "https://example.com"
+
+# ساخت QR code
 img = qrcode.make(data)
-img.save("qrcode.png")
-print("QR code saved as qrcode.png")
+
+# ذخیره تصویر داخل پوشه screenshots
+img.save("screenshots/project02.png")
+
+print("QR code saved to screenshots/project02.png")
